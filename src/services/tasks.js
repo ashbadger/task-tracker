248159eae6a8d8733b1198d8e0d76eb86133bf8c
Ashly <ashlybadgett@gmail.com>
2018-10-28 13:46:14 -0500

    refactor(services): eslint

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -1,39 +1,29 @@
 import db from '../firebase/firebase';
 
-const getTasks = () => {
-	return db.collection('tasks').get()
-		.then(tasks => tasks.docs.map(task => task.data()));
-};
+const getTasks = () => db.collection('tasks').get()
+  .then(tasks => tasks.docs.map(task => task.data()));
 
-const getTask = (id) => {
-	return db.collection('tasks').doc(`/${id}`).get()
-		.then(task => task.data());
-};
+const getTask = id => db.collection('tasks').doc(`/${id}`).get()
+  .then(task => task.data());
 
-const getSubtask = (taskId, subtaskId) => {
-	return db.collection('tasks').doc(`/${taskId}`).get()
-		.then(task => task.data().subtasks.find(subtask => subtask.id == subtaskId));
-};
+const getSubtask = (taskId, subtaskId) => db.collection('tasks').doc(`/${taskId}`).get()
+  .then(task => task.data().subtasks.find(subtask => subtask.id == subtaskId));
 
-const createTask = (task) => {
-	return db.collection('tasks').add(task)
-		.then(task => task.data());
-};
+const createTask = task => db.collection('tasks').add(task)
+  .then(res => res.data());
 
-const updateTask = (id, updates) => {
-	return db.collection('tasks').doc(`/${id}`).update(updates)
-		.then(task => task.data());
-};
+const updateTask = (id, updates) => db.collection('tasks').doc(`/${id}`).update(updates)
+  .then(task => task.data());
 
 const updateSubtasks = (task) => {
-	const { id } = task;
-	return db.collection('tasks').doc(`/${id}`).set(task)
-		.then(task => task.data());	
+  const { id } = task;
+  return db.collection('tasks').doc(`/${id}`).set(task)
+    .then(res => res.data());
 };
 
-const deleteTask = (id) => {
-	return db.collection('tasks').doc(`/${id}`).delete()
-		.then(task => task.data());
-};
+const deleteTask = id => db.collection('tasks').doc(`/${id}`).delete()
+  .then(task => task.data());
 
-export { getTasks, getTask, getSubtask, createTask, updateTask, updateSubtasks, deleteTask };
+export {
+  getTasks, getTask, getSubtask, createTask, updateTask, updateSubtasks, deleteTask,
+};
