0edf63730f31f9284e52fc755bfd1f2ab356f399
Ashly <ashlybadgett@gmail.com>
2018-10-28 22:10:39 -0500

    refactor(app): remove unnecessary duplicate app component

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { injectGlobal } from 'styled-components';
+
+import AppRouter from './routers/AppRouter';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import background from './assets/images/background.png';
+
+injectGlobal`
+  body {
+    margin: 0px;
+    overflow: hidden;
+    height: 100%;
+    font-family: 'Roboto';
+    background-image: url(${background});
+  }
+`;
+const App = () => (
+  <div>
+    <Navbar />
+    <Card>
+      <AppRouter />
+    </Card>
+  </div>
+);
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
