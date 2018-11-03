9a24b481f1f88415070b6c4b0f36eadfd95a4050
Ashly <ashlybadgett@gmail.com>
2018-11-03 18:42:06 -0500

    style(button): add additional styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -16,20 +16,29 @@ const colors = {
 };
 
 export default styled.button`
-  padding: 8px 18px;
-  border-radius: 5px;
-  font-size: small;
   background: ${props => colors[props.color].background};
   border-bottom: solid 3px ${props => colors[props.color]['border-bottom']};
+  border-radius: 5px;
   color: rgba(255, 255, 255, 1);
+  font-size: small;
   font-weight: 600;
+  padding: 8px 18px;
   text-transform: uppercase;
 
   &:hover {
     cursor: pointer;
+    transform: translateY(-1px);
+    margin-bottom: -1px;
+    border-bottom-width: 4px;
   }
 
   &:focus {
     outline: none;
+  }
+
+  &:active {
+    border-bottom: solid 2px ${props => colors[props.color]['border-bottom']};
+    transform: translateY(1px);
+    margin-bottom: 1px;
   }
 `;
