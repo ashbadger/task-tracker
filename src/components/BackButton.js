94b6c88a833d35549d187327d5b2d65f131c3062
Ashly <ashlybadgett@gmail.com>
2018-11-03 18:33:23 -0500

    feat(routing): add animation on route changes

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -4,27 +4,30 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Button = styled.div`
-  position: absolute;
   left: .5rem;
+  position: absolute;
   top: .5rem;
 
-  .button:hover {
+  & svg:hover {
     color: rgba(120, 120, 120, 1) !important;
-    transform: scale(1.1, 1.1);
-    transition-timing-function: ease;
-    transition-duration: 200ms;
     cursor: pointer;
+    transform: scale(1.1, 1.1);
   }
 `;
 
 const propTypes = {
   history: PropTypes.shape({}).isRequired,
+  location: PropTypes.shape({}).isRequired,
 };
 
 class BackButton extends React.Component {
   navigateBack = () => {
     const { history } = this.props;
-    history.goBack();
+    const location = {
+      pathname: history.location.pathname.split('/').slice(0, -1).join('/'),
+      navigateToPrevious: true,
+    };
+    history.push(location);
   }
 
   render() {
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,19 +1,58 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import styled from 'styled-components';
 
 import TaskDetails from '../components/TaskDetails';
 import SubtaskDetails from '../components/SubtaskDetails';
 import TaskList from '../components/TaskList';
+import BackButton from '../components/BackButton';
 
-const AppRouter = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={TaskList} />
-      <Route exact path="/tasks" component={TaskList} />
-      <Route exact path="/tasks/:id" component={TaskDetails} />
-      <Route path="/tasks/:id/:subtaskId" component={SubtaskDetails} />
-    </Switch>
-  </Router>
-);
+const Container = styled.div`
+  position: relative;
+`;
+
+class AppRouter extends React.Component {
+  stripSlashes = string => string.replace('/', '');
+
+  isRootRoute = pathname => ['', 'tasks'].includes(this.stripSlashes(pathname).trim());
+
+  render() {
+    return (
+      <Router>
+        <Route
+          render={({ location, history }) => {
+            const { pathname, key, navigateToPrevious = false } = location;
+            return (
+              <div>
+                {
+                  !this.isRootRoute(pathname)
+                  && <BackButton history={history} location={location} />
+                }
+                <Container>
+                  <TransitionGroup>
+                    <CSSTransition
+                      key={key}
+                      classNames={navigateToPrevious ? 'page--prev' : 'page'}
+                      timeout={800}
+                    >
+                      <Switch location={location}>
+                        <Route exact path="/" component={TaskList} />
+                        <Route exact path="/tasks" component={TaskList} />
+                        <Route exact path="/tasks/:id" component={TaskDetails} />
+                        <Route path="/tasks/:id/:subtaskId" component={SubtaskDetails} />
+                      </Switch>
+                    </CSSTransition>
+                  </TransitionGroup>
+                </Container>
+              </div>
+            );
+          }}
+        />
+      </Router>
+    );
+  }
+}
+
 
 export default AppRouter;
